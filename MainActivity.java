18298942e7cbc0c3ceea649362d114ddaf99cb47
whaktiw <harangjang@g.shingu.ac.kr>
2024-11-05 15:54:51 +0900

    Add files via upload
    예약앱 java소스

---
+++ MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //멤버변수
+    int a;
+
+    Textview Text;
+    //메소드
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(saved)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+TextView text1, text2;
